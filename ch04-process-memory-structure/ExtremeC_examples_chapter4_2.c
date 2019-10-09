ee47313458e48b00390de719cf41b103523a8821
Kamran Amini <kam.cpp@gmail.com>
2019-10-10 00:27:48 +0200

    Update sources of chapter 4.

--- ch04-process-memory-structure/ExtremeC_examples_chapter4_2.c
+++ ch04-process-memory-structure/ExtremeC_examples_chapter4_2.c
@@ -3,7 +3,7 @@
 
 int global_var1;
 int global_var2;
-int global_var3;
+int global_var3 = 0;
 
 int main(int argc, char** argv) {
   return 0;
--- ch04-process-memory-structure/ExtremeC_examples_chapter4_3.c
+++ ch04-process-memory-structure/ExtremeC_examples_chapter4_3.c
@@ -3,7 +3,7 @@
 
 int global_var1;
 int global_var2;
-int global_var3;
+int global_var3 = 0;
 
 double global_var4 = 4.5;
 char global_var5 = 'A';
