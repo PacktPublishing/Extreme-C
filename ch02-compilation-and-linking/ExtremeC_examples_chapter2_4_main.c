bd109bfe6629a145b1af42d92957400a79ad3032
Kamran Amini <kam.cpp@gmail.com>
2019-10-24 20:44:20 +0200

    Update some of the sources of chapter 2.

--- ch02-compilation-and-linking/ExtremeC_examples_chapter2_4_main.c
+++ ch02-compilation-and-linking/ExtremeC_examples_chapter2_4_main.c
@@ -1,7 +1,7 @@
 // File name: ExtremeC_examples_chapter2_4_main.c
 // Description: This file contains the 'main' function.
 
-#include "ExtremeC_examples_chapter2_4_defines.h"
+#include "ExtremeC_examples_chapter2_4_decls.h"
 
 int main(int argc, char** argv) {
   int x = add(4, 5);
